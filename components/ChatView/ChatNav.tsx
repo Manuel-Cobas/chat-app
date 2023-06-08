d8efec4885328ac2b53fb4fa56dc7a8078be6883
Manuel Cobas <cobasguerram@gmail.com>
2023-06-08 14:24:55 -0400

    bug de nombres en ChatNav solucionado (temporalmente)

--- components/ChatView/ChatNav.tsx
+++ components/ChatView/ChatNav.tsx
@@ -1,25 +1,15 @@
 import Image from "next/image";
 import ChatMenu from "./ChatMenu";
 
-import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import useReceiver from "@/hooks/useReceiver";
 import type { ChatNavProps } from "../types";
 
 import { BiArrowBack } from "react-icons/bi"
-import ChatName from "./ChatName";
 
 function ChatNav({ chat }: ChatNavProps) {
   const router = useRouter()
   const { receiver } = useReceiver(chat?.members)
-  const [showInput, setShowInput] = useState<boolean>(false)
-  const [chatName, setChatName] = useState("")
-
-  useEffect(() => {
-    if (chat) {
-      setChatName(chat.name)
-    }
-  }, [chat])
 
   return (
     <header className="fixed top-0 right-0 left-0 h-14">
@@ -44,13 +34,7 @@ function ChatNav({ chat }: ChatNavProps) {
                   width={35}
                 />
               )}
-
-              <ChatName
-                show={showInput}
-                chatName={chatName}
-                setChatName={setChatName}
-                setShowInput={setShowInput}
-              />
+              {receiver && receiver.name}
             </li>
           </ul>
 
